776c97a2b0c94459388518f995a8a468874303b4
jacklocks <castets.eric@free.fr>
2023-10-26 16:39:01 +0200

    formulaire contact et responsive

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -6,15 +6,18 @@ const Contact = () => {
     <div id="contact" className={styles.contactContainer}>
       <h2>contact</h2>
       <div className={styles.contactContent}>
-        <p id="cursor">
-          Pour toute collaboration ou opportunité professionnelle, n'hésitez pas
-          à me contacter <a href="mailto:ecastets.seo@gmail.com">ici</a> !<br />{" "}
-          Je suis ouvert et enthousiaste à l'idée de discuter de projets
-          innovants et de contribuer à des défis stimulants. J'attends avec
-          impatience d'échanger avec vous et de faire partie d'équipes
-          dynamiques pour créer des solutions web ensemble. Merci de votre
-          visite et à bientôt !
-        </p>
+      <p id="cursor"> Pour toute collaboration ou opportunité professionnelle, n'hésitez
+              pas à me contacter!<br/>Je suis ouvert et enthousiaste à l'idée de discuter de projets
+              innovants et de contribuer à des défis stimulants. J'attends avec
+              impatience d'échanger avec vous et de faire partie d'équipes
+              dynamiques pour créer des solutions web ensemble. Merci de me
+              contacter et à bientôt !</p>
+        <div className={styles.contactInput}>
+          <input type="text" id="fullName" placeholder="Nom" required />
+          <input type="email" id="email" placeholder="Email" required />
+          <textarea id="message" placeholder="Message" required></textarea>
+          <button onclick="SendMail()">envoyer</button>
+        </div>
       </div>
     </div>
   );
