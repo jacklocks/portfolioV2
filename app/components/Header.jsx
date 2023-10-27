5e8b73fb324097f1f5705263ced1cffb964c7040
jacklocks <castets.eric@free.fr>
2023-10-27 11:26:57 +0200

    btn admin

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,18 +1,47 @@
-import React from "react";
-import logo from "./Logo";
+"use client";
+import React, { useEffect, useState } from "react";
 import Navigation from "./Navigation";
 import Logo from "./Logo";
-import styles from "../styles/header.module.css"
+import styles from "../styles/header.module.css";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <>
-    <header className={styles.nav}>
-      <Logo />
-      <Navigation />
+      <header className={styles.nav}>
+        <Logo />
+        <Link href="/login-admin" id="adminLink" style={{ display: "none" }}>
+          <h5>admin</h5>
+        </Link>
+        <Navigation />
       </header>
     </>
   );
 };
 
-export default Header;
+const NavbarWithAdminLink = () => {
+  const [typedKeys, setTypedKeys] = useState("");
+
+  useEffect(() => {
+    const adminSequence = process.env.NEXT_PUBLIC_ADMIN_KEY;
+    const handleKeyDown = (event) => {
+      const newTypedKeys = (typedKeys + event.key).slice(-adminSequence.length);
+      setTypedKeys(newTypedKeys);
+    };
+    // Vérifiez si la séquence correspond à "admin"
+    if (typedKeys === adminSequence) {
+      const adminLink = document.getElementById("adminLink");
+      if (adminLink) {
+        adminLink.style.display = "block";
+      }
+    }
+    // Écoute l'événement keydown pour détecter les touches pressées
+    document.addEventListener("keydown", handleKeyDown);
+    // Désinscrivez-vous de l'événement lorsque le composant est démonté
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [typedKeys]);
+  return <Header />;
+};
+export default NavbarWithAdminLink;
